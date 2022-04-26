8b2e17fc3d055191ac1254722d6b51d0fdb12b7e
masendhy <massendhy@gmail.com>
2022-04-26 09:37:17 +0700

    create about page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Josefin: ['Josefin', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'brand-purple': 'var(--clr-purple)',
+        'brand-pink': 'var(--clr-pink)',
+        'brand-yellow': 'var(--clr-yellow)',
+        'brand-blue': 'var(--clr-blue)',
+        'brand-green': 'var(--clr-green)',
+        'brand-light': 'var(--clr-light)',
+        'brand-red': 'var(--clr-red)',
+        'brand-background': 'var(--clr-background)'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
